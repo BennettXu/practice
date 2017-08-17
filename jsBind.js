d5e44e26025c85defd328a95bf628634ebd6ff5f
xujia <jiaxu208939@sohu-inc.com>
2017-08-17 09:43:33 +0800

    Use ...

--- jsBind.js
+++ jsBind.js
@@ -5,7 +5,10 @@ dom.bind = function (fn, context,...arguments) {
     }
     const method = fn;
     const slice = Array.prototype.slice;
-    const args = slice.call(arguments, 2);
+    // when hasn't third params ...arguments, we can use the follow to get relative params
+    // but when has, the arguments stand for reminder params.
+    // const args = slice.call(arguments, 2);
+    const args = arguments;
     return function (...arguments) {
         const array = slice.call(arguments, 0);
         method.apply(context, args.concat(array));
@@ -34,4 +37,5 @@ function run(f) {
 const fx2 = dom.bind(jj.alertName, kk, jj)
 run(fx2);
 const fx3 = dom.bind(obj.fx, obj, 1, 2, 3);
+// line 2's ... stand for [1, 2, 3]
 fx3(4, 5, 6)
