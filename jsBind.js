255887b27e26c2a8a109aa6fb3d1606257f86fd0
xujia <jiaxu208939@sohu-inc.com>
2017-08-17 09:32:32 +0800

    Update code for eslint check

--- jsBind.js
+++ jsBind.js
@@ -1,37 +1,37 @@
-let dom = {};
-dom.bind = function (fn, context) {
+const dom = {};
+dom.bind = function (fn, context,...arguments) {
     if (arguments.length < 2 && context === undefined) {
         return fn;
     }
-    let method = fn;
-    let slice = Array.prototype.slice;
-    let args = slice.call(arguments, 2);
-    return function () {
-        let array = slice.call(arguments, 0);
+    const method = fn;
+    const slice = Array.prototype.slice;
+    const args = slice.call(arguments, 2);
+    return function (...arguments) {
+        const array = slice.call(arguments, 0);
         method.apply(context, args.concat(array));
-    }
+    };
 }
-window.name = "This is Window";
-let obj = {
+window.name = 'This is Window';
+const obj = {
     name: 'A nice demo',
-    fx: function () {
+    fx(...arguments) {
         alert(this.name + '\n' + Array.prototype.slice.call(arguments, 0).join(', '));
-    }
+    },
 };
-let jj = {
+const jj = {
     name: '这是jj',
-    alertName: function () {//绑定失效
+    alertName() { // 绑定失效
         console.log(this.name);
-    }
+    },
 };
-let kk = {
-    name: "kkです",
+const kk = {
+    name: 'kkです',
 }
 function run(f) {
     f();
 }
 // run(jj.alertName);
-let fx2 = dom.bind(jj.alertName)
+const fx2 = dom.bind(jj.alertName, kk, jj)
 run(fx2);
-// var fx2 = dom.bind(obj.fx, obj,1, 2, 3);
-// fx2(4, 5,6);
+const fx3 = dom.bind(obj.fx, obj, 1, 2, 3);
+fx3(4, 5, 6)
